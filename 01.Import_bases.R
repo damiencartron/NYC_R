2f8a872732b77cc7571cd05f81d482ffe458a812
Damien Cartron <damien.cartron@cnrs.fr>
2026-01-29 16:04:29 +0100

    premier test

---
+++ 01.Import_bases.R
@@ -0,0 +1,3 @@
+library(readxl)
+borough_ela_results_2018_2025_public <- read_excel("data/borough-ela-results-2018-2025-public.xlsx", 
+                                                     +     sheet = "ELA - All")
